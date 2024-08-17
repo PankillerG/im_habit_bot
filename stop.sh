f02a7677c54167c8d2f5f1204f9c2f1782beb7b2
pankillerg <vladgusev2000@yandex.ru>
2024-08-17 21:36:22 +0200

    Fix script names in sh files

--- stop.sh
+++ stop.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+SCRIPT_DIR=$( dirname -- $(readlink /usr/bin/stop_im_habit_bot))
 docker compose -f $SCRIPT_DIR/docker-compose.yaml stop
